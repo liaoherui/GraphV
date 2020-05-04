66a0208a593bac38f4a2a10512aa3dde15dad967
Ray <851836818@qq.com>
2020-05-04 18:45:26 +0800

    Update download_2.sh

--- download_2.sh
+++ download_2.sh
@@ -1,1 +1,3 @@
-wget https://bitbucket.org/liao282995/graphv_db/downloads/GraphV_DB.zip
+wget https://bitbucket.org/liao282995/graphv_db/downloads/GraphV_DB.zip &&\
+unzip GraphV_DB.zip &&\
+rm  GraphV_DB.zip
